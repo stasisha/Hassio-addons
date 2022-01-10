1954f7363d2f1d1a4f6ab5e492973279ef0d3cee
stasisha <stasisha@users.noreply.github.com>
2022-01-10 18:39:05 +0200

    Update iptables.sh

--- hotspot_wifi/rootfs/usr/local/bin/iptables.sh
+++ hotspot_wifi/rootfs/usr/local/bin/iptables.sh
@@ -4,3 +4,5 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 netfilter-persistent save
+
+/bin/bash
