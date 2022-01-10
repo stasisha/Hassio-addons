d3cc569131978e1f4f8d71711919a318ba0b5dbe
stasisha <stasisha@users.noreply.github.com>
2022-01-10 19:27:40 +0200

    Update init.sh

--- hotspot_wifi/rootfs/usr/local/bin/init.sh
+++ hotspot_wifi/rootfs/usr/local/bin/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE  
+iptables -D FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -D FORWARD -i wlan0 -o eth0 -j ACCEPT
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
